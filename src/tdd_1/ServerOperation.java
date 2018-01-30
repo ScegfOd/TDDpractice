9fd6c6b46237f18071e239f26e1827da51a282c9
Jonathan Combs <JofCombs@gmail.com>
2018-01-29 23:29:56 -0800

    add shutdown feature

--- src/tdd_1/ServerOperation.java
+++ src/tdd_1/ServerOperation.java
@@ -9,6 +9,7 @@ import tdd_1.RMIInterface;
 public class ServerOperation extends UnicastRemoteObject implements RMIInterface {
 
 	private static final long serialVersionUID = 4865876081371032203L;
+	private static final String mServerName = "TestServer";
 
 	protected ServerOperation() throws RemoteException {
 		super();
@@ -16,18 +17,32 @@ public class ServerOperation extends UnicastRemoteObject implements RMIInterface
 
 	@Override
 	public String helloTo(String name) throws RemoteException {
+		if (name.equals("quit")) {
+			this.shutdown();
+			return "Confirmed, " + mServerName + " is shutting down.";
+		}
 		System.err.println(name + " is trying to make contact!");
 		return "Server says hi to " + name;
 	}
 	
 	public static void main(String[] args) {
 		try {
-			Naming.rebind("//localhost/TestServer", new ServerOperation());
-			System.err.println("The server \"TestServer\" is ready!");
+			Naming.rebind("//localhost/" + mServerName, new ServerOperation());
+			System.err.println("The server \"" + mServerName + "\" is ready!");
 		} catch (Exception e) {
 			System.err.println("server exception: " + e.toString());
 			e.printStackTrace();
 		}
+	}
+	
+	public void shutdown() throws RemoteException {
+		try{
+	        Naming.unbind(mServerName);
+	        UnicastRemoteObject.unexportObject(this, true);
+
+	        System.out.println(mServerName + " is shutting down.");
+	    }
+	    catch(Exception e){}
 	}
 
 }
